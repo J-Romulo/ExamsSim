b53bea536633f94d4140e56dcbda53bca357b832
José Rômulo <joseromulo.10@hotmail.com>
2023-10-15 11:50:38 -0300

    feat: add basic subjectDetails page

---
+++ src/pages/Subjects/SubjectDetails/index.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import { useIsFocused } from '@react-navigation/native';
+import { useStorage } from '../../../hooks/useStorage';
+import { LoadingModal } from '../../../components/LoadingModal';
+import { FlatList, Text, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import * as S from './styles'
+
+export function SubjectDetails({ route }){
+    const { id } = route.params;
+    const {fetchSubject, loading} = useStorage()
+    const [ subject, setSubject ] = useState()
+    const [refreshing, setRefreshing] = useState(false);
+
+    const isFocused = useIsFocused();
+    const { navigate } = useNavigation();
+
+    async function fetchSubjectItem(){
+        const subject = await fetchSubject(id)
+
+        setSubject(subject)
+    }
+
+    useEffect(() => {
+        if(id){
+            fetchSubjectItem()
+        }
+    }, [isFocused, id])
+
+    function onRefresh() {
+        setRefreshing(true);
+        fetchSubjectItem()
+        setRefreshing(false);
+    }
+
+    function RenderQuestion(item){
+        return(
+            <S.ItemContainer
+                onPress={() => {
+                    navigate('question_details', {id: item.item.id})
+                }}
+            >
+                <S.ItemTitle>{item.item.question}</S.ItemTitle>
+            </S.ItemContainer>
+        )
+    }
+
+    function RenderEmptyListMessage(){
+        return(
+            <View style={{ alignItems: "center" }}>
+                <Text>No data found</Text>
+            </View>
+        )
+    }
+
+    return(
+        <S.Container>
+            <LoadingModal isVisible={loading} />
+            <S.FieldLabel>Título</S.FieldLabel>
+            <S.FieldValue>{subject?.title}</S.FieldValue>
+
+            <S.FieldLabel>Descrição</S.FieldLabel>
+            <S.FieldValue>{subject?.description}</S.FieldValue>
+
+            <S.QuestionsHeader>
+                <S.FieldLabel>Questões associadas</S.FieldLabel>
+                <S.CreateQuestionButton
+                    onPress={() => {
+                        navigate('create_question', {id_subject: subject?.id})
+                    }}
+                >
+                    <AntDesign name="plus" size={12} color="black" />
+                </S.CreateQuestionButton>
+            </S.QuestionsHeader>
+            <FlatList 
+                data={subject?.questions}
+                renderItem={RenderQuestion}
+                keyExtractor={(item) => item.id}
+                ListEmptyComponent={RenderEmptyListMessage}
+                onRefresh={onRefresh}
+                refreshing={refreshing}
+            />
+        </S.Container>
+    )
+}
---
+++ src/pages/Subjects/SubjectDetails/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    padding: 20px;
+`
+
+export const FieldLabel = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+`
+
+export const FieldValue = styled.Text`
+    font-size: 16px;
+`
+
+export const QuestionsHeader = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+`
+
+export const CreateQuestionButton = styled.TouchableOpacity`
+    width: 30px;
+    height: 30px;
+    border-radius: 50px;
+    border-style: solid;
+    border-color: gray;
+    border-width: 1px;
+    background-color: white;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const ItemContainer = styled.TouchableOpacity`
+    width: 100%;
+    height: 70px;
+    border-radius: 20px;
+    border-style: solid;
+    border-width: 1px;
+
+    margin-bottom: 20px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const ItemTitle = styled.Text`
+    font-size: 20px;
+    color: black;
+`
