a00750d8d9b436e25e7c47bebb0974cd440fca5d
José Rômulo <joseromulo.10@hotmail.com>
2023-10-15 12:44:15 -0300

    test: husky pre-commit hook test

--- src/pages/Exams/index.jsx
+++ src/pages/Exams/index.jsx
@@ -1,24 +1,22 @@
-import {Text} from 'react-native'
 import { AntDesign } from '@expo/vector-icons';
-import * as S from './styles'
 import { useNavigation } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+import * as S from './styles';
 import { Container } from '../../global/styles/globalComponents';
 
-export function Exams(){
-    const { navigate } = useNavigation();
-    
-    return(
-        <Container>
-            <Text>
-                Simulados
-            </Text>
-            <S.CreateButton
-                onPress={() => {
-                    navigate('create_exam')
-                }}
-            >
-                <AntDesign name="plus" size={24} color="black" />
-            </S.CreateButton>
-        </Container>
-    )
+export function Exams() {
+  const { navigate } = useNavigation();
+
+  return (
+    <Container>
+      <Text>Simulados</Text>
+      <S.CreateButton
+        onPress={() => {
+          navigate('create_exam');
+        }}>
+        <AntDesign name="plus" size={24} color="black" />
+      </S.CreateButton>
+    </Container>
+  );
 }
