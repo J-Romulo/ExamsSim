6f59b0231ebc69d70d4f237d4e016332996ffcfc
José Rômulo <joseromulo.10@hotmail.com>
2023-10-28 12:52:32 -0300

    feat: add custom hook to calculate countdown time

---
+++ src/hooks/useCountdown.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+export function useCountdown({ hours, minutes }) {
+  const countDownDate = new Date();
+  countDownDate.setHours(countDownDate.getHours() + Number(hours));
+  countDownDate.setMinutes(countDownDate.getMinutes() + Number(minutes));
+
+  const [countDown, setCountDown] = useState(countDownDate.getTime() - new Date().getTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const timeRemaining = countDownDate - now;
+
+      if (timeRemaining <= 0) {
+        clearInterval(intervalId);
+        setCountDown(0);
+      } else {
+        setCountDown(timeRemaining);
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [hours]);
+
+  return getReturnValues(countDown);
+}
+
+function getReturnValues(countDown) {
+  // calculate time left
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return [days, hours, minutes, seconds];
+}
