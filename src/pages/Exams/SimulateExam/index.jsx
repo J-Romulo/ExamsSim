2dacb79e29d325abf928f55b0c876ebb084d42bf
José Rômulo <joseromulo.10@hotmail.com>
2023-10-28 14:59:05 -0300

    feat: add time, navigation and finish button to exam page

--- src/pages/Exams/SimulateExam/index.jsx
+++ src/pages/Exams/SimulateExam/index.jsx
@@ -1,83 +1,139 @@
-import * as S from './styles'
-import { useStorage } from "../../../hooks/useStorage"
-import { useState, useEffect } from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import { useState, useEffect } from 'react';
+import { FlatList } from 'react-native';
+
+import { FinishExamModal } from './FinishExamModal';
+import * as S from './styles';
+import { CountdownTimer } from '../../../components/CountdownTimer';
 import { LoadingModal } from '../../../components/LoadingModal';
-import { FlatList, Text, View } from 'react-native'
 import { Container } from '../../../global/styles/globalComponents';
+import { useCountdown } from '../../../hooks/useCountdown';
+import { useStorage } from '../../../hooks/useStorage';
 
-export function SimulateExam({ route }){
-    const { id } = route.params;
-    const {fetchExam, fetchSubjectByTitle, loading} = useStorage()
-    const [ exam, setExam ] = useState()
-    const [questions, setQuestions] = useState([])
-    const [currentQuestion, setCurrentQuestion] = useState()
-    const [currentQuestionindex, setCurrentQuestionIndex] = useState(0)
+export function SimulateExam({ route }) {
+  const { id } = route.params;
+  const { fetchExam, fetchSubjectByTitle, loading } = useStorage();
+  const [exam, setExam] = useState();
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState();
+  const [currentQuestionindex, setCurrentQuestionIndex] = useState(0);
+  const [finishedExam, setFinishedExam] = useState(false);
 
-    async function fetchExamItem(){
-        const exam = await fetchExam(id)
+  async function fetchExamItem() {
+    const exam = await fetchExam(id);
 
-        const questions = await Promise.all(
-            exam.subjects.map(async (subject) => {
-                return await fetchQuestions(subject)
-            })
-        )
+    const questions = await Promise.all(
+      exam.subjects.map(async (subject) => {
+        return await fetchQuestions(subject);
+      })
+    );
 
-        setExam(exam)
-        setQuestions(questions.flat())
-        setCurrentQuestion(questions.flat()[0])
+    setExam(exam);
+    setQuestions(questions.flat());
+    setCurrentQuestion(questions.flat()[0]);
+  }
+
+  async function fetchQuestions(subject_id) {
+    const subject = await fetchSubjectByTitle(subject_id);
+
+    return subject.questions;
+  }
+
+  useEffect(() => {
+    if (id) {
+      fetchExamItem();
     }
+  }, [id]);
 
-    async function fetchQuestions(subject_id){
-        const subject = await fetchSubjectByTitle(subject_id)
+  const [, hours, minutes, seconds] = useCountdown({ hours: exam?.hour, minutes: exam?.minute });
 
-        return subject.questions
-    }
+  function nextQuestion() {
+    if (!questions[currentQuestionindex + 1]) return;
+    setCurrentQuestion(questions[currentQuestionindex + 1]);
+    setCurrentQuestionIndex(currentQuestionindex + 1);
+  }
 
-    useEffect(() => {
-        if(id){
-            fetchExamItem()
-        }
-    }, [id])
+  function lastQuestion() {
+    if (!questions[currentQuestionindex - 1]) return;
+    setCurrentQuestion(questions[currentQuestionindex - 1]);
+    setCurrentQuestionIndex(currentQuestionindex - 1);
+  }
 
-    function nextQuestion(){
-        setCurrentQuestion(questions[currentQuestionindex+1])
-        setCurrentQuestionIndex(currentQuestionindex+1)
-    }
+  function selectAnswer(item) {
+    setQuestions((prevData) =>
+      prevData.map((question, index) =>
+        index === currentQuestionindex ? { ...question, selectedAnswer: item.index } : question
+      )
+    );
 
-    function RenderQuestion(item){
-        return(
-            <S.ItemContainer
-                onPress={() => {
-                    nextQuestion()
-                }}
-            >
-                <S.ItemTitle>{item.item}</S.ItemTitle>
-            </S.ItemContainer>
-        )
-    }
+    setCurrentQuestion((prevData) => {
+      return { ...prevData, selectedAnswer: item.index };
+    });
+  }
 
-    if(!currentQuestion){
-        return(
-            <Container>
-                <LoadingModal isVisible={loading} />
-            </Container>
-        )
-    }
-    return(
-        <Container>
-            <LoadingModal isVisible={loading} />
-            <S.FieldLabel>Questão</S.FieldLabel>
-            <S.FieldValue>{currentQuestion.question}</S.FieldValue>
+  function RenderQuestion(item) {
+    const selectedAnswer = currentQuestion.selectedAnswer === item.index;
 
-            <S.QuestionsHeader>
-                <S.FieldLabel>Alternativas</S.FieldLabel>
-            </S.QuestionsHeader>
+    return (
+      <S.ItemContainer
+        onPress={(e) => {
+          e.preventDefault();
+          selectAnswer(item);
+        }}
+        selected={selectedAnswer}>
+        <S.ItemTitle>{item.item}</S.ItemTitle>
+      </S.ItemContainer>
+    );
+  }
 
-            <FlatList 
-                data={currentQuestion?.answers}
-                renderItem={RenderQuestion}
-                keyExtractor={(item) => item}
-            />
-        </Container>
-    )
+  function finishExam() {
+    setFinishedExam(true);
+  }
+
+  if (!currentQuestion || !exam) {
+    return (
+      <Container>
+        <LoadingModal isVisible={loading} />
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      {!!finishedExam && <FinishExamModal />}
+
+      <S.ExamHeader>
+        <S.ArrowButton onPress={lastQuestion}>
+          <AntDesign name="arrowleft" size={24} color="black" />
+        </S.ArrowButton>
+
+        <S.QuestionsCount>{`${currentQuestionindex + 1}/${questions.length}`}</S.QuestionsCount>
+
+        {!!(hours && minutes && seconds) && (
+          <CountdownTimer hours={hours} minutes={minutes} seconds={seconds} />
+        )}
+
+        <S.ArrowButton onPress={nextQuestion}>
+          <AntDesign name="arrowright" size={24} color="black" />
+        </S.ArrowButton>
+      </S.ExamHeader>
+
+      <LoadingModal isVisible={loading} />
+      <S.FieldLabel>Questão</S.FieldLabel>
+      <S.FieldValue>{currentQuestion.question}</S.FieldValue>
+
+      <S.QuestionsHeader>
+        <S.FieldLabel>Alternativas</S.FieldLabel>
+      </S.QuestionsHeader>
+
+      <FlatList
+        data={currentQuestion?.answers}
+        renderItem={RenderQuestion}
+        keyExtractor={(item) => item}
+      />
+
+      <S.FinishButton onPress={finishExam}>
+        <S.ButtonText>Finalizar teste</S.ButtonText>
+      </S.FinishButton>
+    </Container>
+  );
 }
--- src/pages/Exams/SimulateExam/styles.js
+++ src/pages/Exams/SimulateExam/styles.js
@@ -1,36 +1,71 @@
 import styled from 'styled-components/native';
 
 export const FieldLabel = styled.Text`
-    font-size: 16px;
-    font-weight: bold;
-`
+  font-size: 16px;
+  font-weight: bold;
+`;
 
 export const FieldValue = styled.Text`
-    font-size: 16px;
-`
+  font-size: 16px;
+`;
 
 export const QuestionsHeader = styled.View`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: row;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
 
 export const ItemContainer = styled.TouchableOpacity`
-    width: 100%;
-    height: 70px;
-    border-radius: 20px;
-    border-style: solid;
-    border-width: 1px;
+  width: 100%;
+  height: 70px;
+  border-radius: 20px;
+  border-style: solid;
+  border-width: 1px;
 
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${(props) => (props.selected ? props.theme.colors.primary : 'white')};
+`;
 
 export const ItemTitle = styled.Text`
-    font-size: 20px;
-    color: black;
-`
+  font-size: 20px;
+  color: black;
+`;
+
+export const ExamHeader = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const ArrowButton = styled.TouchableOpacity``;
+
+export const QuestionsCount = styled.Text``;
+
+export const FinishButton = styled.TouchableOpacity`
+  width: 50%;
+  height: 40px;
+  border-radius: 10px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+
+  margin-top: 20px;
+
+  background-color: #e14741;
+`;
+
+export const ButtonText = styled.Text`
+  color: white;
+`;
