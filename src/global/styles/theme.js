6818168fa7fd34035e4f710b1fef31b1e213fbf5
José Rômulo <joseromulo.10@hotmail.com>
2023-11-11 17:26:05 -0300

    style: add more global colors

--- src/global/styles/theme.js
+++ src/global/styles/theme.js
@@ -1,6 +1,9 @@
 export default {
-    colors: {
-      primary: '#095FD9',
-      secondary: '#E0EDFF'
-    }
-  }
+  colors: {
+    primary: '#095FD9',
+    secondary: '#E0EDFF',
+
+    green: '#09D964',
+    red: '#e14741',
+  },
+};
