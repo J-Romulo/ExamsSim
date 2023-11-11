3d63fd1d39b766144a1893bc2ee284c79140109b
José Rômulo <joseromulo.10@hotmail.com>
2023-11-11 17:27:11 -0300

    feat: add table showing exam results in modal

--- src/pages/Exams/SimulateExam/FinishExamModal/index.jsx
+++ src/pages/Exams/SimulateExam/FinishExamModal/index.jsx
@@ -1,16 +1,42 @@
 import { AntDesign } from '@expo/vector-icons';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-native-modal';
 
 import * as S from './styles';
+import { LoadingModal } from '../../../../components/LoadingModal';
 
-export function FinishExamModal() {
-  const [isModalVisible, setModalVisible] = useState(true);
+
+export function FinishExamModal({ questions, isModalVisible, setModalVisible }) {
+  const [correctAnswers, setCorrectAnswers] = useState(0);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
 
+  useEffect(() => {
+    let correctAnswersToParse = 0;
+    questions &&
+      questions.length > 0 &&
+      questions.forEach((question) => {
+        if (question.selectedAnswer && question.selectedAnswer == question.correct_answer)
+          correctAnswersToParse++;
+      });
+
+    setCorrectAnswers(correctAnswersToParse);
+  }, [questions]);
+
+  if (!questions) {
+    return (
+      <Modal visible={isModalVisible} hasBackdrop backdropColor="black" backdropOpacity={0.4}>
+        <S.Container>
+          <S.CloseButton onPress={toggleModal}>
+            <AntDesign name="close" size={24} color="white" />
+          </S.CloseButton>
+          <LoadingModal isVisible />
+        </S.Container>
+      </Modal>
+    );
+  }
   return (
     <Modal visible={isModalVisible} hasBackdrop backdropColor="black" backdropOpacity={0.4}>
       <S.Container>
@@ -19,7 +45,48 @@ export function FinishExamModal() {
         </S.CloseButton>
 
         <S.TitleText>Simulado finalizado!</S.TitleText>
-        <S.TitleText>Confira seu resultado:</S.TitleText>
+        <S.TitleText>Resultado:</S.TitleText>
+
+        <S.TitleText>
+          Você acertou {correctAnswers} de {questions?.length}
+        </S.TitleText>
+
+        <S.AnswersTable>
+          <S.TableHeader>
+            <S.TableColumn style={{ flex: 0.75 }}>
+              <S.TableHeaderTitle>Questão</S.TableHeaderTitle>
+            </S.TableColumn>
+
+            <S.TableColumn>
+              <S.TableHeaderTitle>Selecionada</S.TableHeaderTitle>
+            </S.TableColumn>
+
+            <S.TableColumn>
+              <S.TableHeaderTitle>Correta</S.TableHeaderTitle>
+            </S.TableColumn>
+          </S.TableHeader>
+
+          {questions.map((question, index) => {
+            const backgroundColor = index % 2 === 0 ? '#FFF' : '#b7d5f0';
+            return (
+              <S.TableRow style={{ backgroundColor }}>
+                <S.TableColumn style={{ flex: 0.5 }}>
+                  <S.TableData>#{index + 1}</S.TableData>
+                </S.TableColumn>
+
+                <S.TableColumn>
+                  <S.TableData>
+                    {question.selectedAnswer ? Number(question.selectedAnswer) + 1 : null}
+                  </S.TableData>
+                </S.TableColumn>
+
+                <S.TableColumn>
+                  <S.TableData>{Number(question.correct_answer) + 1}</S.TableData>
+                </S.TableColumn>
+              </S.TableRow>
+            );
+          })}
+        </S.AnswersTable>
       </S.Container>
     </Modal>
   );
--- src/pages/Exams/SimulateExam/FinishExamModal/styles.js
+++ src/pages/Exams/SimulateExam/FinishExamModal/styles.js
@@ -22,6 +22,9 @@ export const Container = styled.View`
   elevation: 22;
 
   border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${(props) => props.theme.colors.secondary};
 `;
 
 export const TitleText = styled.Text`
@@ -39,4 +42,56 @@ export const CloseButton = styled.TouchableOpacity`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+export const AnswersTable = styled.View`
+  flex: 1;
+  margin-top: 12px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0 5px;
+  background-color: ${(props) => props.theme.colors.secondary};
+  border-radius: 5px;
+
+  height: 100%;
+`;
+
+export const TableHeader = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding: 0 5px;
+
+  border-radius: 5px;
+  margin-bottom: 5px;
+  border-bottom-color: #fff;
+  border-bottom-width: 2px;
+
+  & {
+    font-weight: bold;
+  }
+`;
+
+export const TableRow = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding: 0 5px;
+
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+export const TableColumn = styled.View``;
+
+export const TableHeaderTitle = styled.Text`
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const TableData = styled.Text`
+  font-size: 15px;
 `;
