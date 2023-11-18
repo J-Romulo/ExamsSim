fc1ac11d1f2c22f2ea4778b248fcdb6115b42c8d
José Rômulo <joseromulo.10@hotmail.com>
2023-11-18 13:50:42 -0300

    style: add padding-bottom to global container component

--- src/global/styles/globalComponents.js
+++ src/global/styles/globalComponents.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 10px 20px 0 20px;
+  padding: 10px 20px 15px 20px;
   background-color: ${({ theme }) => theme.colors.secondary};
 `;
 
