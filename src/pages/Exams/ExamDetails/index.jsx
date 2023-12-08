4a6c1d39f90b33f73d50ed23af1fd3901013bc10
José Rômulo <joseromulo.10@hotmail.com>
2023-12-08 15:52:18 -0300

    feat: add subjects and results list to exam details

--- src/pages/Exams/ExamDetails/index.jsx
+++ src/pages/Exams/ExamDetails/index.jsx
@@ -1,11 +1,22 @@
+import { AntDesign } from '@expo/vector-icons';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
+import { FlatList, ScrollView, LayoutAnimation, UIManager } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import * as S from './styles';
+import { EmptyContent } from '../../../components/EmptyContent';
 import { LoadingModal } from '../../../components/LoadingModal';
-import { Container } from '../../../global/styles/globalComponents';
+import {
+  Container,
+  FloatingActionButton,
+  ItemContainer,
+} from '../../../global/styles/globalComponents';
+import { useDialogModal } from '../../../hooks/useDialogModal';
 import { useStorage } from '../../../hooks/useStorage';
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export function ExamDetails({ route }) {
   const { id } = route.params;
@@ -13,11 +24,12 @@ export function ExamDetails({ route }) {
   const theme = useTheme();
   const [loading, setLoading] = useState(false);
 
-  const { fetchExam } = useStorage();
+  const { openTwoOptionsModal } = useDialogModal();
+  const { fetchExamWithSubjects, dissociateSubjectFromExam } = useStorage();
   const [exam, setExam] = useState();
   const isFocused = useIsFocused();
 
-  const [subjectsListOpen, setSubjectListOpen] = useState(false);
+  const [subjectsListOpen, setSubjectListOpen] = useState(true);
   const [questionsListOpen, setQuestionsListOpen] = useState(false);
   const [historyListOpen, setHistoryListOpen] = useState(false);
 
@@ -25,7 +37,7 @@ export function ExamDetails({ route }) {
 
   async function fetchExamItem() {
     setLoading(true);
-    const exam = await fetchExam(id);
+    const exam = await fetchExamWithSubjects(id);
 
     setExam(exam);
     setLoading(false);
@@ -37,12 +49,133 @@ export function ExamDetails({ route }) {
     }
   }, [isFocused, id]);
 
+  function handleDesassociateSubject(subject_title) {
+    openTwoOptionsModal(
+      'Tem certeza que deseja desassociar a matéria?',
+      'Sim',
+      'Cancelar',
+      async () => {
+        await dissociateSubjectFromExam(id, subject_title);
+        fetchExamItem();
+      }
+    );
+  }
+
+  function handleSubjectsMenu() {
+    // Define the animation configuration
+    const config = LayoutAnimation.create(
+      200, // duration in milliseconds
+      LayoutAnimation.Types.easeInEaseOut,
+      LayoutAnimation.Properties.scaleY // specify the property to animate
+    );
+
+    // Trigger the layout animation
+    LayoutAnimation.configureNext(config);
+
+    setSubjectListOpen(!subjectsListOpen);
+  }
+
+  function handleHistoricMenu() {
+    // Define the animation configuration
+    const config = LayoutAnimation.create(
+      200, // duration in milliseconds
+      LayoutAnimation.Types.easeInEaseOut,
+      LayoutAnimation.Properties.scaleY // specify the property to animate
+    );
+
+    // Trigger the layout animation
+    LayoutAnimation.configureNext(config);
+
+    setHistoryListOpen(!historyListOpen);
+  }
+
+  function RenderSubjects(item) {
+    return (
+      <>
+        <ItemContainer
+          onPress={() => {
+            navigate('subject_details', { id: item.item.id });
+          }}>
+          <S.ItemTitle>{item.item.title}</S.ItemTitle>
+        </ItemContainer>
+
+        <S.DeleteButton onPress={() => handleDesassociateSubject(item.item.title)}>
+          <AntDesign name="minus" size={12} color="white" />
+        </S.DeleteButton>
+      </>
+    );
+  }
+
+  function RenderHistoric(item) {
+    return (
+      <>
+        <ItemContainer>
+          <S.ItemTitle>{item.item.correct_answers}</S.ItemTitle>
+        </ItemContainer>
+      </>
+    );
+  }
+
   return (
     <Container>
-      <LoadingModal isVisible={loading} />
+      <ScrollView>
+        <LoadingModal isVisible={loading} />
 
-      <S.ExamTitle>{exam?.title}</S.ExamTitle>
-      <S.ExamDescription>{exam?.description}</S.ExamDescription>
+        <S.ExamTitle>{exam?.title}</S.ExamTitle>
+        <S.ExamDescription>{exam?.description}</S.ExamDescription>
+
+        <S.SubjectListContainer>
+          <S.SubjectListBttn onPress={handleSubjectsMenu}>
+            <S.SubjectListTitle>Matérias associadas</S.SubjectListTitle>
+            <AntDesign
+              name={subjectsListOpen ? 'caretup' : 'caretdown'}
+              size={25}
+              color={theme.colors.primary_text}
+            />
+          </S.SubjectListBttn>
+
+          {subjectsListOpen && (
+            <FlatList
+              data={exam?.subjects}
+              renderItem={RenderSubjects}
+              keyExtractor={(item) => item.id}
+              ListEmptyComponent={EmptyContent({ emptyText: 'Nenhuma matéria encontrada' })}
+              initialNumToRender={20}
+              maxToRenderPerBatch={10}
+              getItemLayout={(_, index) => ({ length: 90, offset: 90 * index, index })}
+            />
+          )}
+        </S.SubjectListContainer>
+
+        <S.SubjectListContainer>
+          <S.SubjectListBttn onPress={handleHistoricMenu}>
+            <S.SubjectListTitle>Histórico</S.SubjectListTitle>
+            <AntDesign
+              name={historyListOpen ? 'caretup' : 'caretdown'}
+              size={25}
+              color={theme.colors.primary_text}
+            />
+          </S.SubjectListBttn>
+
+          {historyListOpen && (
+            <FlatList
+              data={exam?.results}
+              renderItem={RenderHistoric}
+              keyExtractor={(item) => item.id}
+              ListEmptyComponent={EmptyContent({ emptyText: 'Nenhum histórico encontrado' })}
+              initialNumToRender={20}
+              maxToRenderPerBatch={10}
+              getItemLayout={(_, index) => ({ length: 90, offset: 90 * index, index })}
+            />
+          )}
+        </S.SubjectListContainer>
+      </ScrollView>
+      <FloatingActionButton
+        onPress={() => {
+          navigate('simulate_exam', { id: exam.id });
+        }}>
+        <AntDesign name="playcircleo" size={24} color={theme.colors.text_on_background} />
+      </FloatingActionButton>
     </Container>
   );
 }
--- src/pages/Exams/ExamDetails/styles.js
+++ src/pages/Exams/ExamDetails/styles.js
@@ -10,3 +10,51 @@ export const ExamDescription = styled.Text`
   font-size: 16px;
   color: ${({ theme }) => theme.colors.secondary_text};
 `;
+
+export const ItemTitle = styled.Text`
+  font-size: 20px;
+  color: ${(props) => props.theme.colors.text_on_background};
+`;
+
+export const SubjectListContainer = styled.View`
+  display: flex;
+  max-height: 490px;
+  width: 100%;
+
+  padding-top: 10px;
+`;
+
+export const SubjectListTitle = styled.Text`
+  font-size: 22px;
+  margin-bottom: 4px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primary_text};
+`;
+
+export const SubjectListBttn = styled.TouchableOpacity`
+  width: 100%;
+  height: 50px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DeleteButton = styled.TouchableOpacity`
+  width: 25px;
+  height: 25px;
+  border-radius: 50px;
+  border-style: solid;
+  border-color: ${({ theme }) => theme.colors.buttons_background};
+  border-width: 1px;
+  background-color: ${({ theme }) => theme.colors.red};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  right: 5px;
+  top: 5px;
+`;
